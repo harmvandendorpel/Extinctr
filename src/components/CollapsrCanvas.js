186eae25e3765de506475ca1500f8cde035a4dc3
harmvandendorpel <mail@harmvandendorpel.com>
2017-06-18 13:35:14 +0200

    simplify a component

--- src/components/CollapsrCanvas.js
+++ src/components/CollapsrCanvas.js
@@ -1,48 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class CollapsrCanvas extends Component {
-  static propTypes = {
-    // image: PropTypes.object.isRequired,
-    // scatter: PropTypes.number.isRequired,
-    // playing: PropTypes.bool.isRequired,
-    // addFrame: PropTypes.func.isRequired,
-    // recording: PropTypes.bool.isRequired,
-    // fixedColor: PropTypes.array.isRequired
-    onCanvasReady: PropTypes.func.isRequired
-  };
+const CollapsrCanvas = ({ onCanvasReady }) =>
+  <canvas className={'faller'} ref={canvas => onCanvasReady(canvas)} />;
 
-  componentDidMount() {
-    // faller = createFaller(
-    //   this.canvas,
-    //   this.props.image,
-    //   this.props.fixedColor,
-    //   this.props.scatter
-    // );
-    //
-    // faller.init();
-    // this.removed = false;
-    // this.update();
-    this.props.onCanvasReady(this.canvas);
-  }
+CollapsrCanvas.propTypes = {
+  onCanvasReady: PropTypes.func.isRequired
+};
 
-  // componentWillUnmount() {
-    // this.removed = true;
-  // }
-
-  // update() {
-    // if (this.removed) return;
-    // window.requestAnimationFrame(this.update.bind(this));
-    // if (!this.props.playing) return;
-    // // faller.update();
-    // if (this.props.recording) {
-    //   // this.props.addFrame(this.canvas);
-    // }
-  // }
-
-  render() {
-    return (
-      <canvas className={'faller'} ref={(canvas) => { this.canvas = canvas; }} />
-    );
-  }
-}
+export default CollapsrCanvas;
