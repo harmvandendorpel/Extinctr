bbbe13b7a828e525192e1a07949efb01523e944c
harmvandendorpel <mail@harmvandendorpel.com>
2017-06-18 13:25:59 +0200

    more eslint changes

--- src/containers/Root.dev.js
+++ src/containers/Root.dev.js
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import App from './App';
 // import DevTools from './DevTools';
 
-module.exports = class Root extends Component {
-  static propTypes = {
-    store: PropTypes.object.isRequired
-  };
+function Root({ store }) {
+  return (
+    <Provider store={store}>
+      <div>
+        <App />
+        {/* <DevTools /> */}
+      </div>
+    </Provider>
+  );
+}
 
-  render() {
-    const { store } = this.props;
-    return (
-      <Provider store={store}>
-        <div>
-          <App />
-          {/* <DevTools /> */}
-        </div>
-      </Provider>
-    );
-  }
+Root.propTypes = {
+  store: PropTypes.object.isRequired
 };
+
+module.exports = Root;
--- src/containers/Root.prod.js
+++ src/containers/Root.prod.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import App from './App';
 
-module.exports = class Root extends Component {
-  static propTypes = {
-    store: PropTypes.object.isRequired
-  };
-
-  render() {
-    const { store } = this.props;
-    return (
-      <Provider store={store}>
+function Root({ store }) {
+  return (
+    <Provider store={store}>
+      <div>
         <App />
-      </Provider>
-    );
-  }
+      </div>
+    </Provider>
+  );
+}
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired
 };
+
+module.exports = Root;
